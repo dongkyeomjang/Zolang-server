6488406e29c6ab1c9a608f4eff18767f9db344d8
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-04-15 16:23:32 +0900

    feat: cluster domain 구현

---
+++ src/main/java/com/kcs/zolang/domain/Cluster.java
@@ -0,0 +1,52 @@
+package com.kcs.zolang.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cluster")
+public class Cluster {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cluster_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "cluster_name", nullable = false)
+    private String clusterName;
+
+    @Column(name = "secret_token", nullable = false)
+    private String secretToken;
+
+    @Column(name = "domain_url", nullable = false)
+    private String domainUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Cluster(User user, String clusterName, String secretToken, String domainUrl) {
+        this.user = user;
+        this.clusterName = clusterName;
+        this.secretToken = secretToken;
+        this.domainUrl = domainUrl;
+        this.createdAt = LocalDateTime.now();
+    }
+    public void update(String clusterName, String secretToken, String domainUrl) {
+        this.clusterName = clusterName;
+        this.secretToken = secretToken;
+        this.domainUrl = domainUrl;
+    }
+}
