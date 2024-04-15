f1e6fcddd486e39234b919575089a705d07f58e7
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-04-15 16:41:34 +0900

    fix: add AccessLevel.PROTECTED on repository

--- src/main/java/com/kcs/zolang/domain/Repository.java
+++ src/main/java/com/kcs/zolang/domain/Repository.java
@@ -1,6 +1,7 @@
 package com.kcs.zolang.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "repository")
 public class Repository {
     @Id
