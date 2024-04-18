33c6e07624337f516bdbc477032e7ad0fd689775
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-04-18 17:22:10 +0900

    fix: 쿠키 도메인 설정 변경

--- src/test/java/com/kcs/zolang/ZolangApplicationTests.java
+++ src/test/java/com/kcs/zolang/ZolangApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class AreYouSleepingApplicationTests {
+public class ZolangApplicationTests {
     @Test
     void contextLoads() {
     }
