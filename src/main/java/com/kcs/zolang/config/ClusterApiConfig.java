667f2d211edbd146ed6b42357bb07d04c85e8d31
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-04-23 14:29:45 +0900

    feat: add ClusterApiConfig

--- src/main/java/com/kcs/zolang/config/ClusterApiConfig.java
+++ src/main/java/com/kcs/zolang/config/ClusterApiConfig.java
@@ -1,2 +1,13 @@
-package com.kcs.zolang.config;public class ClusterApiConfig {
+package com.kcs.zolang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClusterApiConfig {
+    @Bean
+    public RestTemplate clusterApiRestTemplate() {
+        return new RestTemplate();
+    }
 }
