c06f87e85e3942c2aa618b6b5b00d86aa4519d40
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-04-29 17:54:56 +0900

    feat: 클러스터의 노드 정보 조회 로직 구현

--- src/main/java/com/kcs/zolang/service/ClusterService.java
+++ src/main/java/com/kcs/zolang/service/ClusterService.java
@@ -1,32 +1,60 @@
 package com.kcs.zolang.service;
 
 import com.kcs.zolang.domain.Cluster;
+import com.kcs.zolang.domain.User;
+import com.kcs.zolang.dto.request.RegisterClusterDto;
 import com.kcs.zolang.dto.response.ClusterListDto;
 import com.kcs.zolang.exception.CommonException;
 import com.kcs.zolang.exception.ErrorCode;
 import com.kcs.zolang.repository.ClusterRepository;
+import com.kcs.zolang.repository.UserRepository;
 import com.kcs.zolang.utility.ClusterApiUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.List;
-
+import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class ClusterService {
     private final ClusterRepository clusterRepository;
     private final ClusterApiUtil clusterApiUtil;
+    private final UserRepository userRepository;
 
-    public List<ClusterListDto> getClusterList(Long userId) {
+    public Long registerCluster(Long userId, MultipartFile file, RegisterClusterDto registerClusterDto) {
+        try {
+            InputStream certificateStream = file.getInputStream();
+            String filename = file.getOriginalFilename();
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+            return clusterRepository.save(clusterApiUtil.registerNewCluster(user, registerClusterDto, certificateStream, filename)).getId();
+        } catch (Exception e) {
+            throw new CommonException(ErrorCode.CLUSTER_REGISTRATION_FAILED);
+        }
+    }
+    public List<ClusterListDto> getClusters(Long userId) {
         return clusterRepository.findByUserId(userId).stream() // 사용자가 웹서비스에 등록해놓은 클러스터 리스트 가져와서
                 .map(ClusterListDto::fromEntity) // ClusterListDto로 변환하고
                 .toList(); // List로 합친 후 리턴. 반환값: clusterName, domainUrl, version(DB에 저장되어있는 값들만) Status를 위한 값은 추가로 구현해야함(저장 값 먼저 출력 후, 약간 시간이 걸릴 수 있는 status는 로딩 후 나타나게)
     }
 
-    public Boolean getClusterStatus(Long clusterId) {
+    public Boolean getClusterStatus(Long clusterId) throws Exception {
         // 클러스터의 상태를 가져오는 메소드. 클러스터의 상태는 DB에 저장되어있지 않고, 실시간으로 가져와야함.
         Cluster cluster = clusterRepository.findById(clusterId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_CLUSTER));
-        return clusterApiUtil.getClusterStatus(cluster.getDomainUrl(), cluster.getSecretToken());
+        RestTemplate restTemplate = clusterApiUtil.createRestTemplateForCluster(cluster);
+        return clusterApiUtil.getClusterStatus(cluster.getDomainUrl(), cluster.getSecretToken(), restTemplate);
     }
+
+    public List<Map<String,Object>> getClusterNodes(Long clusterId) throws Exception {
+        // 클러스터에 등록된 노드 목록을 가져오는 메소드. 클러스터의 노드 목록은 DB에 저장되어있지 않고, 실시간으로 가져와야함.
+        Cluster cluster = clusterRepository.findById(clusterId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_CLUSTER));
+        RestTemplate restTemplate = clusterApiUtil.createRestTemplateForCluster(cluster);
+        return clusterApiUtil.getClusterNodes(cluster.getDomainUrl(), cluster.getSecretToken(), restTemplate);
+    }
+
 }
--- src/main/java/com/kcs/zolang/utility/ClusterApiUtil.java
+++ src/main/java/com/kcs/zolang/utility/ClusterApiUtil.java
@@ -1,20 +1,99 @@
 package com.kcs.zolang.utility;
 
+import com.kcs.zolang.config.SSLConfig;
+import com.kcs.zolang.domain.Cluster;
+import com.kcs.zolang.domain.User;
+import com.kcs.zolang.dto.request.RegisterClusterDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.ByteArrayInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ClusterApiUtil {
-    private final RestTemplate restTemplate;
+    // private final SSLConfig sslConfigurator;
 
-    public boolean getClusterStatus(String url, String token) {
+    // public RestTemplate createRestTemplateForCluster(Cluster cluster) throws Exception {
+    //     SSLContext sslContext = createSSLContext(cluster.getCertPath());
+    //     CloseableHttpClient httpClient = HttpClients.custom()
+    //             .setConnectionManager(
+    //                     PoolingHttpClientConnectionManagerBuilder.create()
+    //                             .setSSLSocketFactory(
+    //                                     SSLConnectionSocketFactoryBuilder.create()
+    //                                             .setSslContext(sslContext)
+    //                                             .build())
+    //                             .build())
+    //             .build();
+    //     HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    //     return new RestTemplate(requestFactory);
+    // }
+
+    // public Cluster registerNewCluster(User user, RegisterClusterDto clusterDto, InputStream certificateStream, String filename) throws Exception {
+    //     String certPath = sslConfigurator.storeCertificate(certificateStream, filename, user.getId(), clusterDto.clusterName());
+    //     return Cluster.builder()
+    //             .clusterName(clusterDto.clusterName())
+    //             .domainUrl(clusterDto.domainUrl())
+    //             .secretToken(clusterDto.secretToken())
+    //             .user(user)
+    //             .version(clusterDto.version())
+    //             .certPath(certPath)
+    //             .build();
+    // }
+    // public SSLContext createSSLContext(String certPath) throws Exception {
+    //     try {
+    //         log.info("Certificate file path : {}", certPath);
+
+    //         // PEM 형식의 파일을 읽기
+    //         FileReader fileReader = new FileReader(certPath);
+    //         PemReader pemReader = new PemReader(fileReader);
+    //         ByteArrayInputStream inputStream = new ByteArrayInputStream(pemReader.readPemObject().getContent());
+
+    //         // CertificateFactory를 사용하여 X.509 형식의 인증서 생성
+    //         CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+    //         X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(inputStream);
+    //         pemReader.close();
+
+    //         // KeyStore에 인증서 추가
+    //         KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    //         keyStore.load(null, null);
+    //         keyStore.setCertificateEntry("certificate", certificate);
+
+    //         TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+    //         tmf.init(keyStore);
+
+    //         SSLContext sslContext = SSLContext.getInstance("TLS");
+    //         sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+    //         return sslContext;
+    //     } catch (Exception e) {
+    //         log.error("Failed to load certificate file", e);
+    //         throw e;
+    //     }
+    // }
+
+    public boolean getClusterStatus(String url, String token, RestTemplate restTemplate) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -33,6 +112,25 @@ public class ClusterApiUtil {
         return false;
     }
 
+    public List<Map<String,Object>> getClusterNodes(String url, String token, RestTemplate restTemplate) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                .path("/api/v1/nodes");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<Map> response = restTemplate.exchange(
+                builder.build().encode().toUri(), HttpMethod.GET, entity, Map.class);
+
+        if (response.getStatusCode() == HttpStatus.OK && response.hasBody()) {
+            Map<String, Object> body = response.getBody();
+            return extractNodesDetails(body);
+        }
+        return List.of();
+    }
+
     private Boolean checkNodesStatus(Map<String,Object> nodesInfo) {
         if (nodesInfo != null && nodesInfo.containsKey("items")) {
             List<Map<String, Object>> items = (List<Map<String, Object>>) nodesInfo.get("items");
@@ -45,5 +143,32 @@ public class ClusterApiUtil {
             }
         }
         return true;
+    }
+    private List<Map<String, Object>> extractNodesDetails(Map<String, Object> nodesInfo) {
+        List<Map<String, Object>> result = new ArrayList<>();  // 빈 리스트 생성 대신 ArrayList 인스턴스를 생성
+        if (nodesInfo != null && nodesInfo.containsKey("items")) {
+            List<Map<String, Object>> items = (List<Map<String, Object>>) nodesInfo.get("items");
+            for (Map<String, Object> item : items) {
+                Map<String, Object> nodeDetails = new HashMap<>();  // 각 노드의 상세 정보를 저장할 Map 생성
+                Map<String, Object> metadata = (Map<String, Object>) item.get("metadata");
+                Map<String, Object> status = (Map<String, Object>) item.get("status");
+                Map<String, Object> nodeInfo = (Map<String, Object>) status.get("nodeInfo");
+
+                nodeDetails.put("created", metadata.get("creationTimestamp"));
+                nodeDetails.put("name", metadata.get("name"));
+                nodeDetails.put("addresses", status.get("addresses"));
+                nodeDetails.put("capacity", status.get("capacity"));
+                nodeDetails.put("allocatable", status.get("allocatable"));
+                nodeDetails.put("conditions", status.get("conditions"));
+                nodeDetails.put("OS", nodeInfo.get("operatingSystem"));
+                nodeDetails.put("OSImage", nodeInfo.get("osImage"));
+                nodeDetails.put("kernelVersion", nodeInfo.get("kernelVersion"));
+                nodeDetails.put("containerRuntime", nodeInfo.get("containerRuntimeVersion"));
+                nodeDetails.put("KubeletVersion", nodeInfo.get("kubeletVersion"));
+
+                result.add(nodeDetails);  // 개별 노드의 상세 정보를 결과 리스트에 추가
+            }
+        }
+        return result;  // 모든 노드의 상세 정보가 담긴 리스트 반환
     }
 }
