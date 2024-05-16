dc403039a9737ebce3cfb2179d11d4d809fc73ba
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-05-16 13:46:36 +0900

    feat: handleGithubWebhook controller 구현

--- src/main/java/com/kcs/zolang/controller/WebhookController.java
+++ src/main/java/com/kcs/zolang/controller/WebhookController.java
@@ -1,2 +1,24 @@
-package com.kcs.zolang.controller;public class WebhookController {
+package com.kcs.zolang.controller;
+
+import com.kcs.zolang.dto.global.ResponseDto;
+import com.kcs.zolang.service.CICDService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/webhook")
+public class WebhookController {
+    private final CICDService cicdService;
+
+    @PostMapping("")
+    public ResponseDto<?> handleGithubWebhook(@RequestBody Map<String, Object> payload) {
+        cicdService.handleGithubWebhook(payload);
+        return ResponseDto.ok(null);
+    }
 }
