d3696ed42fd26175e12ed6ef8ad7b27f7c648a2d
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-05-16 14:49:55 +0900

    fix: Add Error 'FAILED_CREATE_WEBHOOK'

--- src/main/java/com/kcs/zolang/exception/ErrorCode.java
+++ src/main/java/com/kcs/zolang/exception/ErrorCode.java
@@ -53,7 +53,8 @@ public enum ErrorCode {
     UPLOAD_FILE_ERROR(50001, HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패하였습니다."),
     FILE_STORAGE_ERROR(50001, HttpStatus.INTERNAL_SERVER_ERROR, "파일 저장에 실패하였습니다."),
     CLUSTER_REGISTRATION_FAILED(50002, HttpStatus.INTERNAL_SERVER_ERROR, "클러스터 등록에 실패하였습니다."),
-    API_ERROR(50003, HttpStatus.INTERNAL_SERVER_ERROR, "API 호출에 실패하였습니다.");
+    API_ERROR(50003, HttpStatus.INTERNAL_SERVER_ERROR, "API 호출에 실패하였습니다."),
+    FAILED_CREATE_WEBHOOK(50004, HttpStatus.INTERNAL_SERVER_ERROR, "Webhook 생성에 실패하였습니다.");
 
     private final Integer code;
     private final HttpStatus httpStatus;
