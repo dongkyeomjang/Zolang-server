78cc5f4861a99768c0819a3c3301e1212da8bcf6
Chae <oumuamua121@gmail.com>
2024-05-21 00:06:37 +0900

    BBB-89 설명 추가

--- src/main/java/com/kcs/zolang/dto/response/DeploymentDetailDto.java
+++ src/main/java/com/kcs/zolang/dto/response/DeploymentDetailDto.java
@@ -6,12 +6,17 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-@Schema(name = "DeploymentDetailDto", description = "배포 상세 Dto")
+@Schema(name = "DeploymentDetailDto", description = "디폴로이먼트 상세 Dto")
 public record DeploymentDetailDto(
+    @Schema(description = "deployment 메타데이터")
     CommonMetadataDto metadata,
+    @Schema(description = "deployment 리소스")
     DeploymentResourceDto resource,
+    @Schema(description = "롤링 업데이트 정책")
     RolingUpdateStrategyDto rollingUpdateStrategy,
+    @Schema(description = "pod 상태")
     DeploymentPodStatusDto podConditions,
+    @Schema(description = "deployment 상태들")
     List<DeploymentConditionDto> condition
 ) {
 
