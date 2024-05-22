266cdfb7a46e3fbbbe0438c1f5ce218ba0d98271
kimhyoseon <kimhyoseon928@gmail.com>
2024-05-22 09:49:19 +0900

    Update NetworkController.java
    fix: 파일 수정 오류 되돌림

--- src/main/java/com/kcs/zolang/controller/NetworkController.java
+++ src/main/java/com/kcs/zolang/controller/NetworkController.java
@@ -1,71 +1,42 @@
-package com.kcs.zolang.dto.response.network;
+package com.kcs.zolang.controller;
 
-import io.kubernetes.client.openapi.models.V1Service;
-import io.kubernetes.client.openapi.models.V1ServicePort;
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.kcs.zolang.annotation.UserId;
+import com.kcs.zolang.dto.global.ResponseDto;
+import com.kcs.zolang.service.NetworkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/network")
+@Validated
+@Tag(name="Monitoring-Network API", description="모니터링-네트워크 API")
+public class NetworkController {
+    private final NetworkService networkService;
 
-import static com.kcs.zolang.utility.MonitoringUtil.getAge;
+    @GetMapping("/{clusterId}/service")
+    @Operation(summary = "Network-service 목록 조회", description = "사용자의 네트워크-서비스 목록 조회")
+    public ResponseDto<?> getServiceList(
+            @UserId Long userId,
+            @PathVariable(name = "clusterId") Long clusterId
+    ) {
+        return ResponseDto.ok(networkService.getServiceList(userId, clusterId));
+    }
 
-@Schema(name = "ServiceDetailDto", description = "유저 Service Detail 정보 Dto")
-public record ServiceDetailDto(
-        @Schema(description = "MetaData") MetaData metaData,
-        @Schema(description = "Spec") Spec spec,
-        @Schema(description = "Status") Status status) {
+    @GetMapping("/{clusterId}/service/{serviceName}")
+    @Operation(summary = "Network-service 목록 조회", description = "사용자의 네트워크-서비스 상세 조회")
+    public ResponseDto<?> getServiceDetail(
+            @UserId Long userId,
+            @PathVariable(name = "clusterId") Long clusterId,
+            @PathVariable(name = "serviceName") String serviceName
+    ) {
+        return ResponseDto.ok(networkService.getServiceDetail(userId, clusterId, serviceName));
+    }
 
-        @Schema(name = "MetaData", description = "Service MetaData")
-        public record MetaData(
-                @Schema(description = "Network(service) 생성일", example = "2024-03-20T23:26:36+9:00") String serviceTimeStamp,
-                @Schema(description = "Network(service) 생성 시간", example = "5 day") String serviceAge,
-                @Schema(description = "Network(service) 이름", example = "kubernetes") String serviceName,
-                @Schema(description = "Network(service) 네임스페이스", example = "default") String serviceNamespace,
-                @Schema(description = "Network(service) 레이블", example = "app: cert-manager,") Map<String, String> serviceLabels,
-                @Schema(description = "Network(service) 어노테이션", example = "kubectl.kubernetes.io/last-applied-configuratio : {apiversion1}") Map<String, String> serviceAnnotations) {
-        }
-
-        @Schema(name = "Spec", description = "Service Spec")
-        public record Spec(
-                @Schema(description = "Network(service) 포트", example = "[9402]") List<Integer> servicePort,
-                @Schema(description = "Network(service) 셀렉터", example = "{app./compo: controller,..}") Map<String, String> serviceSelector,
-                @Schema(description = "Network(service) 타입", example = "ClusterIP") String serviceType,
-                @Schema(description = "Network(service) 클러스터 IP", example = "10.100.158.205") String serviceClusterIp,
-                @Schema(description = "Network(service) IP 패밀리", example = "[Ipv4]") List<String> serviceIpFamiles,
-                @Schema(description = "Network(service) IP 패밀리 정책", example = "SingleStack") String serviceIpFamilyPolicy) {
-        }
-
-        @Schema(name = "Status", description = "Service Status")
-        public record Status(
-                @Schema(description = "Network(service) 상태") Map<String, String> serviceStatus) {
-        }
-
-        public static ServiceDetailDto fromEntity(V1Service service) {
-                MetaData metaData = new MetaData(
-                        service.getMetadata().getCreationTimestamp().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                        getAge(Objects.requireNonNull(service.getMetadata().getCreationTimestamp()).toLocalDateTime()),
-                        service.getMetadata().getName(),
-                        service.getMetadata().getNamespace(),
-                        service.getMetadata().getLabels(),
-                        service.getMetadata().getAnnotations()
-                );
-
-                Spec spec = new Spec(
-                        service.getSpec().getPorts().stream().map(V1ServicePort::getPort).collect(Collectors.toList()),
-                        service.getSpec().getSelector(),
-                        service.getSpec().getType(),
-                        service.getSpec().getClusterIP(),
-                        service.getSpec().getIpFamilies(),
-                        service.getSpec().getIpFamilyPolicy()
-                );
-
-                Status status = new Status(
-                        null // This line can be modified based on the actual status information you want to include
-                );
-
-                return new ServiceDetailDto(metaData, spec, status);
-        }
 }
