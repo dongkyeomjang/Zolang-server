d8e0db766188f33674b37694927a1f37e2e846ff
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-05-31 23:01:31 +0900

    feat: INVALID_PAYLOAD 추가

--- src/main/java/com/kcs/zolang/exception/ErrorCode.java
+++ src/main/java/com/kcs/zolang/exception/ErrorCode.java
@@ -35,6 +35,7 @@ public enum ErrorCode {
     BAD_REQUEST_PARAMETER(40005, HttpStatus.BAD_REQUEST, "잘못된 요청 파라미터입니다."),
     BAD_REQUEST_JSON(40006, HttpStatus.BAD_REQUEST, "잘못된 JSON 형식입니다."),
     SEARCH_SHORT_LENGTH_ERROR(40007, HttpStatus.BAD_REQUEST, "검색어는 2글자 이상이어야 합니다."),
+    INVALID_PAYLOAD(40008, HttpStatus.BAD_REQUEST, "유효하지 않은 페이로드입니다."),
 
 
     // Access Denied Error
