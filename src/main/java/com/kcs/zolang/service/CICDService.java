ba33f203c745950c98165b06c0b39a599ea818ac
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-05-31 22:57:06 +0900

    fix: lastCommitMessage 잘못된 참조 수정

--- src/main/java/com/kcs/zolang/service/CICDService.java
+++ src/main/java/com/kcs/zolang/service/CICDService.java
@@ -118,9 +118,8 @@ public class CICDService {
     public void handleGithubWebhook(Map<String, Object> payload) {
         try {
             String repoName = (String) ((Map<String, Object>) payload.get("repository")).get("name");
-            Map<String,Object> checkSuite = (Map<String, Object>) payload.get("check_suite");
-            String branch = (String) checkSuite.get("head_branch");
-            String lastCommitMessage =(String) ((Map<String,Object>) checkSuite.get("head_commit")).get("message");
+            String branch = (String) ((Map<String, Object>) payload.get("check_suite")).get("head_branch");
+            String lastCommitMessage = (String) ((Map<String,Object>)((Map<String, Object>) payload.get("check_suite")).get("head_commit")).get("message");
             CICD cicd = cicdRepository.findByRepositoryName(repoName)
                     .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_REPOSITORY));
 
