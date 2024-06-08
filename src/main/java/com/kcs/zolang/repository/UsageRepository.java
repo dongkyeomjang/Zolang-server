56154b0d3fc82d83979b6c28440d992c688c3748
Chae <oumuamua121@gmail.com>
2024-06-08 21:50:26 +0900

    BBB-144 중복 저장 처리

--- src/main/java/com/kcs/zolang/repository/UsageRepository.java
+++ src/main/java/com/kcs/zolang/repository/UsageRepository.java
@@ -1,17 +1,17 @@
 package com.kcs.zolang.repository;
 
 import com.kcs.zolang.domain.Usage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsageRepository extends JpaRepository<Usage, Long> {
-    List<Usage> findAllByUserIdAndCreatedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
+
+    List<Usage> findAllByUserIdAndCreatedAtBetween(Long userId, LocalDateTime start,
+        LocalDateTime end);
 
     // usage 데이터 삭제
     void deleteByCreatedAtBefore(LocalDateTime thresholdDate);
+
+    boolean existsByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
